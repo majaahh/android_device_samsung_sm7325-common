8d0ce191c3fd64f5b1653f319c652d422d5854a4
Majaahh <maja.blaszczak@proton.me>
2026-06-19 22:48:58 +0200

    sm7325: Reformat custom C++ code with clang
    Change-Id: I9026a6c49deacfd589878d954c7c70efc7e104c4
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- libinit/init_sm7325.h
+++ libinit/init_sm7325.h
@@ -17,37 +17,18 @@ typedef struct {
     std::string codename;
 } variant;
 
-static const variant international_models_a52sxq = {
-    .model = "SM-A528B",
-    .codename = "a52sxq"
-};
+static const variant international_models_a52sxq = {.model = "SM-A528B", .codename = "a52sxq"};
 
-static const variant asia_models_a52sxq = {
-    .model = "SM-A528N",
-    .codename = "a52sxq"
-};
+static const variant asia_models_a52sxq = {.model = "SM-A528N", .codename = "a52sxq"};
 
-static const variant international_models_m52xq = {
-    .model = "SM-M526BR",
-    .codename = "m52xq"
-};
+static const variant international_models_m52xq = {.model = "SM-M526BR", .codename = "m52xq"};
 
-static const variant america_models_m52xq = {
-    .model = "SM-M526B",
-    .codename = "m52xq"
-};
+static const variant america_models_m52xq = {.model = "SM-M526B", .codename = "m52xq"};
 
-static const variant international_models_a73xq = {
-    .model = "SM-A736B",
-    .codename = "a73xq"
-};
+static const variant international_models_a73xq = {.model = "SM-A736B", .codename = "a73xq"};
 
-static const variant *all_variants[VARIANT_MAX] = {
-    &international_models_a52sxq,
-    &asia_models_a52sxq,
-    &international_models_m52xq,
-    &america_models_m52xq,
-    &international_models_a73xq
-};
+static const variant* all_variants[VARIANT_MAX] = {
+        &international_models_a52sxq, &asia_models_a52sxq, &international_models_m52xq,
+        &america_models_m52xq, &international_models_a73xq};
 
-#endif // INIT_SM7325_H
+#endif  // INIT_SM7325_H
