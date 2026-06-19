18b5b04f723a5c3fd0d638cba4cfadd47de1ca74
Majaahh <maja.blaszczak@proton.me>
2026-06-19 22:29:34 +0200

    sm7325: Refactor
    Change-Id: I5883b453777455a5fb625189249d228d23bff785
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ products/sec_nfc.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# NFC
+PRODUCT_PACKAGES += android.hardware.nfc-service.sec
+
+# NFC - Configuration
+PRODUCT_PACKAGES += libnfc-sec-vendor.conf
