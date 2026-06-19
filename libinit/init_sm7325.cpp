c6e4895c55d4bdab85a08a51af47881ad125079c
Majaahh <maja.blaszczak@proton.me>
2026-06-19 22:48:58 +0200

    sm7325: Reformat custom C++ code with clang
    Change-Id: I9026a6c49deacfd589878d954c7c70efc7e104c4
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- libinit/init_sm7325.cpp
+++ libinit/init_sm7325.cpp
@@ -16,25 +16,24 @@
 #define MODEL_NAME_LEN_M52 6
 
 static void property_override(char const prop[], char const value[]) {
-    prop_info *pi;
+    prop_info* pi;
 
-    pi = (prop_info*) __system_property_find(prop);
+    pi = (prop_info*)__system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
     else
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_quad(char const system_prop[], char const vendor_prop[],  char const product_prop[], char const odm_prop[], char const value[])
-{
+void property_override_quad(char const system_prop[], char const vendor_prop[],
+                            char const product_prop[], char const odm_prop[], char const value[]) {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
     property_override(product_prop, value);
     property_override(odm_prop, value);
 }
 
-void vendor_load_properties()
-{
+void vendor_load_properties() {
     const std::string bootloader = android::base::GetProperty("ro.bootloader", "");
     std::string bl_model;
     if (bootloader.substr(0, MODEL_NAME_LEN_M52) == "M526BR") {
@@ -70,8 +69,11 @@ void vendor_load_properties()
     LOG(INFO) << "Setting ro.product.name: %s", name.c_str();
     LOG(INFO) << "Setting ro.build.product: %s", device.c_str();
 
-    property_override_quad("ro.product.model", "ro.product.vendor.model", "ro.product.product.model", "ro.product.odm.model", model.c_str());
-    property_override_quad("ro.product.device", "ro.product.vendor.device", "ro.product.product.device", "ro.product.odm.device", device.c_str());
-    property_override_quad("ro.product.name", "ro.product.vendor.name", "ro.product.product.name", "ro.product.odm.name", name.c_str());
+    property_override_quad("ro.product.model", "ro.product.vendor.model",
+                           "ro.product.product.model", "ro.product.odm.model", model.c_str());
+    property_override_quad("ro.product.device", "ro.product.vendor.device",
+                           "ro.product.product.device", "ro.product.odm.device", device.c_str());
+    property_override_quad("ro.product.name", "ro.product.vendor.name", "ro.product.product.name",
+                           "ro.product.odm.name", name.c_str());
     property_override("ro.build.product", device.c_str());
 }
--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -19,33 +19,31 @@
 #include "edify/expr.h"
 #include "otautil/error_code.h"
 
-Value *VerifyBootloader(const char *name, State *state,
-                        const std::vector<std::unique_ptr<Expr>> &argv) {
-  int ret = 0;
-  std::string bootloader = android::base::GetProperty("ro.boot.bootloader", "");
-  if (bootloader.empty()) {
-    return ErrorAbort(state, kFileGetPropFailure,
-                      "%s() failed to read current bootloader version", name);
-  }
+Value* VerifyBootloader(const char* name, State* state,
+                        const std::vector<std::unique_ptr<Expr>>& argv) {
+    int ret = 0;
+    std::string bootloader = android::base::GetProperty("ro.boot.bootloader", "");
+    if (bootloader.empty()) {
+        return ErrorAbort(state, kFileGetPropFailure,
+                          "%s() failed to read current bootloader version", name);
+    }
 
-  std::string min_supported_bootloader_arg;
-  if (argv.empty() || !Evaluate(state, argv[0], &min_supported_bootloader_arg)) {
-    return ErrorAbort(state, kArgsParsingFailure,
-                      "%s() error parsing arguments", name);
-  }
-  int min_supported_bootloader = int(min_supported_bootloader_arg[0]);
+    std::string min_supported_bootloader_arg;
+    if (argv.empty() || !Evaluate(state, argv[0], &min_supported_bootloader_arg)) {
+        return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
+    }
+    int min_supported_bootloader = int(min_supported_bootloader_arg[0]);
 
-  int version = 0;
-  if (bootloader.length() >= 4)
-    version = int(bootloader[bootloader.length() - 4]);
+    int version = 0;
+    if (bootloader.length() >= 4) version = int(bootloader[bootloader.length() - 4]);
 
-  if (version >= min_supported_bootloader) {
-    ret = 1;
-  }
+    if (version >= min_supported_bootloader) {
+        ret = 1;
+    }
 
-  return StringValue(std::to_string(ret));
+    return StringValue(std::to_string(ret));
 }
 
 void Register_librecovery_updater_samsung_sm7325() {
-  RegisterFunction("samsung_sm7325.verify_bootloader_min", VerifyBootloader);
+    RegisterFunction("samsung_sm7325.verify_bootloader_min", VerifyBootloader);
 }
