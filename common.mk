d05d14b4002656f0e05494ebbbf0c80de37bdd82
Majaahh <maja.blaszczak@proton.me>
2026-06-22 12:19:39 +0200

    sm7325: Setup Dolby
    Change-Id: I1b977bd8fd4108d7ebfab35cb404e0a88b01bbd0
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -56,6 +56,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Audio - Effects - Dolby
+PRODUCT_PACKAGES += SamsungDAP
+
 # Audio - Effects - FX
 TARGET_EXCLUDES_AUDIOFX := true
 
