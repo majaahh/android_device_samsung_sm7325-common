5032ed829f3145a917c5e61a142027ba810b2c82
Majaahh <maja.blaszczak@proton.me>
2026-06-22 14:23:45 +0200

    sm7325: Drop recovery updater
    * Unecessary for my personal usage
    
    Change-Id: I996aa4d3d92ad386cfb9a425a6b5dc411dabb572
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- releasetools.py
+++ releasetools.py
@@ -15,32 +15,14 @@
 # limitations under the License.
 
 import common
-import re
-
-def FullOTA_Assertions(info):
-  OTA_Assertions(info, info.input_zip)
-  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def IncrementalOTA_Assertions(info):
-  OTA_Assertions(info, info.input_zip)
-  return
-
 def IncrementalOTA_InstallEnd(info):
   info.input_zip = info.target_zip
   OTA_InstallEnd(info)
-  return
-
-def OTA_Assertions(info, input_zip):
-  android_info = input_zip.read("OTA/android-info-extra.txt")
-  m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info.decode('utf-8'))
-  if m:
-    bootloader_version = m.group(1)
-    cmd = ('assert(samsung_sm7325.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires Android 13 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
-    info.script.AppendExtra(cmd)
   return
 
 def AddImage(info, basename, dest):
