03582c0bed8f577f72c9dc25f76affae549920bc
Majaahh <maja.blaszczak@proton.me>
2026-06-22 09:26:37 +0200

    sm7325: Exclude AudioFX
    Change-Id: I0022f7a9b2572a70ab38f3c5378b658fd797030f
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -52,6 +52,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Audio - FX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Audio - Sound Trigger - Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
