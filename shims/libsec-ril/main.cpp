8f2fbc32d44e53baae9aa7b95d8875f18f571c24
Flopster101 <nahuelgomez329@gmail.com>
2026-08-19 23:06:02 +0200

    sm7325: Properly format SMSC instead of nulling it
    Change-Id: I4609eb3f4cf66f042797371d506f17f5d31f1220
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- shims/libsec-ril/main.cpp
+++ shims/libsec-ril/main.cpp
@@ -6,6 +6,9 @@
 #include <dlfcn.h>
 
 #include <cstddef>
+#include <mutex>
+#include <string>
+#include <unordered_map>
 
 #include <log/log.h>
 #include <telephony/ril.h>
@@ -14,12 +17,8 @@
 #error "REAL_LIB_NAME must be defined by Android.bp"
 #endif
 
-#ifndef RIL_REQUEST_SEND_SMS
-#define RIL_REQUEST_SEND_SMS 25
-#endif
-
-#ifndef RIL_REQUEST_SEND_SMS_EXPECT_MORE
-#define RIL_REQUEST_SEND_SMS_EXPECT_MORE 26
+#ifndef RIL_REQUEST_GET_SMSC_ADDRESS
+#define RIL_REQUEST_GET_SMSC_ADDRESS 100
 #endif
 
 static constexpr char kRealPath[] = "/vendor/lib64/" REAL_LIB_NAME;
@@ -40,6 +39,11 @@ using RilInit = const RIL_RadioFunctions* (*)(const RIL_Env*, int, char**);
 
 static void* gRealHandle = nullptr;
 static SamsungRequestFunc gRealOnRequest = nullptr;
+static const RIL_Env* gRealEnv = nullptr;
+static RIL_Env gShimEnv;
+
+static std::mutex gTokenMapMutex;
+static std::unordered_map<RIL_Token, int> gTokenRequestMap;
 
 static void* getRealHandle() {
     if (gRealHandle != nullptr) {
@@ -72,35 +76,152 @@ static RilInit getRealInit(const char* name) {
     return realInit;
 }
 
-static bool isCsSmsRequest(int request) {
-    return request == RIL_REQUEST_SEND_SMS || request == RIL_REQUEST_SEND_SMS_EXPECT_MORE;
+static int parseHexByte(const std::string& str, size_t start) {
+    if (start + 2 > str.length()) {
+        return -1;
+    }
+    char c1 = str[start];
+    char c2 = str[start + 1];
+    int val1 = (c1 >= '0' && c1 <= '9')   ? (c1 - '0')
+               : (c1 >= 'a' && c1 <= 'f') ? (c1 - 'a' + 10)
+               : (c1 >= 'A' && c1 <= 'F') ? (c1 - 'A' + 10)
+                                          : -1;
+    int val2 = (c2 >= '0' && c2 <= '9')   ? (c2 - '0')
+               : (c2 >= 'a' && c2 <= 'f') ? (c2 - 'a' + 10)
+               : (c2 >= 'A' && c2 <= 'F') ? (c2 - 'A' + 10)
+                                          : -1;
+    if (val1 == -1 || val2 == -1) {
+        return -1;
+    }
+    return (val1 << 4) | val2;
+}
+
+static bool isHexString(const std::string& str) {
+    if (str.length() < 12) {
+        return false;
+    }
+    for (char c : str) {
+        if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool isDecimalString(const std::string& str) {
+    if (str.length() < 10) {
+        return false;
+    }
+    for (char c : str) {
+        if (!(c >= '0' && c <= '9')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static std::string decodeBcdSmsc(const std::string& pdu) {
+    if (pdu.length() < 4) {
+        return "";
+    }
+
+    int len = parseHexByte(pdu, 0);
+    if (len < 0) {
+        return "";
+    }
+
+    if (pdu.length() < static_cast<size_t>(2 + len * 2)) {
+        return "";
+    }
+
+    int toa = parseHexByte(pdu, 2);
+    if (toa < 0) {
+        return "";
+    }
+
+    std::string num = "";
+    for (size_t i = 4; i < static_cast<size_t>(2 + len * 2); i += 2) {
+        if (i + 1 >= pdu.length()) {
+            break;
+        }
+        char c1 = pdu[i + 1];
+        char c2 = pdu[i];
+        if (c1 != 'F' && c1 != 'f') {
+            num += c1;
+        }
+        if (c2 != 'F' && c2 != 'f') {
+            num += c2;
+        }
+    }
+
+    if ((toa & 0xF0) == 0x90) {
+        num = "+" + num;
+    }
+    return num;
+}
+
+static std::string fixSmscAddress(const std::string& origSmsc) {
+    std::string smsc = origSmsc;
+
+    if (smsc.find('"') != std::string::npos || smsc.find(',') != std::string::npos) {
+        size_t firstQuote = smsc.find('"');
+        if (firstQuote != std::string::npos) {
+            size_t secondQuote = smsc.find('"', firstQuote + 1);
+            if (secondQuote != std::string::npos) {
+                smsc = smsc.substr(firstQuote + 1, secondQuote - firstQuote - 1);
+            }
+        }
+    }
+
+    if (isHexString(smsc) && smsc.rfind("07", 0) == 0) {
+        std::string decoded = decodeBcdSmsc(smsc);
+        if (!decoded.empty()) {
+            smsc = decoded;
+        }
+    } else {
+        if (isDecimalString(smsc) && smsc.rfind("+", 0) != 0) {
+            smsc = "+" + smsc;
+        }
+    }
+
+    return smsc;
 }
 
 static void shimOnRequest(int request, void* data, size_t datalen, RIL_Token token,
                           RIL_SOCKET_ID socketId) {
-    if (isCsSmsRequest(request) && data != nullptr && datalen >= 2 * sizeof(char*)) {
-        const char** smsData = static_cast<const char**>(data);
-        const char* smsc = smsData[0];
-        const char* pdu = smsData[1];
-
-        if (pdu != nullptr) {
-            const char* fixedSmsData[] = {
-                    nullptr,
-                    pdu,
-            };
-
-            ALOGI("sec-ril-shim: request %d: forcing CS SMSC NULL, "
-                  "original smsc=%s",
-                  request, smsc != nullptr ? smsc : "null");
-            gRealOnRequest(request, const_cast<char**>(fixedSmsData), sizeof(fixedSmsData), token,
-                           socketId);
-            return;
-        }
-
-        ALOGW("sec-ril-shim: request %d: null PDU", request);
+    {
+        std::lock_guard<std::mutex> lock(gTokenMapMutex);
+        gTokenRequestMap[token] = request;
     }
 
     gRealOnRequest(request, data, datalen, token, socketId);
+}
+
+static void shimOnRequestComplete(RIL_Token token, RIL_Errno error, void* response,
+                                  size_t responselen) {
+    int request = -1;
+    {
+        std::lock_guard<std::mutex> lock(gTokenMapMutex);
+        auto it = gTokenRequestMap.find(token);
+        if (it != gTokenRequestMap.end()) {
+            request = it->second;
+            gTokenRequestMap.erase(it);
+        }
+    }
+
+    if (request == RIL_REQUEST_GET_SMSC_ADDRESS && error == RIL_E_SUCCESS && response != nullptr &&
+        responselen > 0) {
+        const char* originalSmsc = static_cast<const char*>(response);
+        std::string fixedSmsc = fixSmscAddress(originalSmsc);
+
+        ALOGI("sec-ril-shim: GET_SMSC_ADDRESS response: %s -> %s", originalSmsc, fixedSmsc.c_str());
+
+        gRealEnv->OnRequestComplete(token, error, const_cast<char*>(fixedSmsc.c_str()),
+                                    fixedSmsc.length() + 1);
+        return;
+    }
+
+    gRealEnv->OnRequestComplete(token, error, response, responselen);
 }
 
 extern "C" const RIL_RadioFunctions* RIL_Init(const RIL_Env* env, int argc, char** argv) {
@@ -109,7 +230,11 @@ extern "C" const RIL_RadioFunctions* RIL_Init(const RIL_Env* env, int argc, char
         return nullptr;
     }
 
-    const RIL_RadioFunctions* real = realRilInit(env, argc, argv);
+    gRealEnv = env;
+    gShimEnv = *env;
+    gShimEnv.OnRequestComplete = shimOnRequestComplete;
+
+    const RIL_RadioFunctions* real = realRilInit(&gShimEnv, argc, argv);
     if (real == nullptr) {
         ALOGE("sec-ril-shim: invalid real RIL function table");
         return real;
@@ -130,7 +255,7 @@ extern "C" const RIL_RadioFunctions* RIL_Init(const RIL_Env* env, int argc, char
         return nullptr;
     }
 
-    ALOGI("sec-ril-shim: installed in-place CS SMSC NULL shim");
+    ALOGI("sec-ril-shim: installed SMSC format shim");
     return real;
 }
 
