26dd59df558701038c978c08ba74e74bd6eda693
Majaahh <maja.blaszczak@proton.me>
2026-08-30 23:57:53 +0200

    sm7325: Shim libhypervintf
    Change-Id: I62b976efe86cb172d52da005a0a261a38fe1fcd6
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ shims/libhypervintf/hypervintf.cpp
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <vector>
+
+struct HyPerToken {};
+
+void hyper_acquire(HyPerToken, std::__1::vector<int, std::__1::allocator<int>>) {}
+
+void hyper_checkResourceExist(int) {}
+
+void hyper_getNewHyPerToken() {}
+
+void hyper_getSupportedFrequency(int, int) {}
+
+void hyper_release(HyPerToken) {}
+
+void hyper_setProcName(
+        std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char>>) {}
